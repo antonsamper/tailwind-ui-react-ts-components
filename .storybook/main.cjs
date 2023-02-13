e9025eec50d060bcf8265b37e45ac4896c3b1937
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2023-02-13 16:00:59 +0000

    upgrading to sb7

---
+++ .storybook/main.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: [{
+        loader: "postcss-loader",
+        options: {
+          implementation: require.resolve("postcss")
+        }
+      }],
+      include: path.resolve(__dirname, "../")
+    });
+    // Return the altered config
+    return config;
+  },
+  docs: {
+    autodocs: true
+  }
+};

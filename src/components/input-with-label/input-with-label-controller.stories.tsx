5fd21681bc5bfbe95ef4ebcae98af5fc44bb7cb1
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2023-03-22 21:31:04 +0000

    removing interaction plugins

--- src/components/input-with-label/input-with-label-controller.stories.tsx
+++ src/components/input-with-label/input-with-label-controller.stories.tsx
@@ -17,10 +17,7 @@ export const InputWithLabelControllerStory: Story = {
     name: 'Input with label controller',
     render: () => {
         // eslint-disable-next-line react-hooks/rules-of-hooks
-        const { control, handleSubmit } = useForm<{
-            email: string;
-            name: string;
-        }>({
+        const { control, handleSubmit } = useForm<{ email: string; name: string }>({
             defaultValues: { email: 'jim.halpert@theoffice.com', name: 'Jim Halpert' },
         });
 
@@ -28,7 +25,9 @@ export const InputWithLabelControllerStory: Story = {
             <form className="space-y-4" role="form" onSubmit={handleSubmit(action('onSubmit'))}>
                 <InputWithLabelController control={control} name="name" label="Name" placeholder="John Doe" />
                 <InputWithLabelController control={control} name="email" label="Email" placeholder="you@example.com" type="email" />
-                <input className="rounded-md bg-indigo-50 py-2 px-3 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100" type="submit" />
+                <button className="rounded-md bg-indigo-50 py-2 px-3 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100" type="submit">
+                    Submit
+                </button>
             </form>
         );
     },

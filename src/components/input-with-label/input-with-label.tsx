e22dc1b6f4f2f68ca5d848e4915930dd3d871fdd
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2023-04-05 15:12:31 +0100

    tweak input styling

--- src/components/input-with-label/input-with-label.tsx
+++ src/components/input-with-label/input-with-label.tsx
@@ -7,13 +7,13 @@ export const InputWithLabel = forwardRef<HTMLInputElement, InputWithLabelPropert
 
     return (
         <div data-testid="component-input-with-label">
-            <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+            <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">
                 {label}
             </label>
             <div className="mt-1">
                 <input
                     id={name}
-                    className={`block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ${className}`}
+                    className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${className}`}
                     type={type || 'text'}
                     ref={reference}
                     {...attributes}

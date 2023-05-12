079073dfebfa96f81c0dd76eac609b054c913aa1
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2023-05-12 15:01:27 +0100

    additional theming setup

---
+++ .storybook/preview.tsx
@@ -0,0 +1,28 @@
+import '../src/tailwind.css';
+
+import { withThemeByDataAttribute } from '@storybook/addon-styling';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  withThemeByDataAttribute({
+    attributeName: 'data-mode',
+    defaultTheme: 'light',
+    themes: {
+      dark: 'dark',
+      light: 'light',
+    },
+  }),
+];
+
+// Set additional theming classes
+document.querySelector('html').classList.add('h-full');
+document.body.classList.add('min-h-screen', 'bg-white', 'dark:bg-slate-900');

1321391206a2ac49fc7d9348aaa4c9d2cd33d97f
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2023-05-12 13:27:05 +0100

    update storybook tailwind

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,25 +1,18 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y", {
+    name: '@storybook/addon-styling',
+    options: {
+      // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+      // For more details on this addon's options.
+      postCss: true,
+    },
+  }],
   "framework": {
     name: "@storybook/react-vite",
     options: {}
   },
   "features": {
     "storyStoreV7": true
-  },
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.css$/i,
-      use: [{
-        loader: "postcss-loader",
-        options: {
-          implementation: require.resolve("postcss")
-        }
-      }],
-      include: path.resolve(__dirname, "../")
-    });
-    // Return the altered config
-    return config;
   }
 };

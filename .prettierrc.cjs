755e00b7cedce72c7cdaef4e56f920e06bd94a82
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2023-09-05 22:22:36 +0100

    fix dependencies

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-    plugins: [require('prettier-plugin-tailwindcss')],
+    plugins: ['prettier-plugin-tailwindcss'],
     printWidth: 160,
     singleQuote: true,
     tabWidth: 4,
--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,15 @@
-module.exports = {
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
   addons: ['@storybook/addon-a11y', '@storybook/addon-essentials', '@storybook/addon-links', '@storybook/addon-storysource', '@storybook/addon-styling'],
   features: {
     storyStoreV7: true,
   },
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
+  framework: '@storybook/react-vite',
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  core: {
+    disableTelemetry: true,
+  },
 };
+
+export default config;
